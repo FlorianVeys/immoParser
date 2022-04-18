a14f9a4149435d94d26f6e3c92fedfba7267bc23
Florian Veys <florian.veys@qualifio.com>
2022-04-18 23:17:31 +0200

    feat: define date of creation and date of update

--- src/dao/homesDao.py
+++ src/dao/homesDao.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+from distutils.log import error
+import sys
 from typing import Set, List, Dict
+from pymongo.errors import DuplicateKeyError
 
 from infrastructure.mongoManager import MongoManager
 from model.home import Home
@@ -13,8 +17,31 @@ class HomesDao:
     def parse_homes(self, homes: Set[Home]) -> List[Dict]:
         homes_parsed = []
         for home in homes:
-            homes_parsed.append(home.__dict__)
+            home_dict = home.__dict__
+            home_dict["created_at"] = datetime.utcnow().isoformat()
+            home_dict["updated_at"] = datetime.utcnow().isoformat()
+
+            homes_parsed.append(home_dict)
         return homes_parsed
     
     def save(self, homes: Set[Home]) -> None:
-        self.collection.insert_many(self.parse_homes(homes))
+        homes_parsed = self.parse_homes(homes)
+
+        for home in homes_parsed:
+            try:
+                self.collection.insert_one(home)
+            except DuplicateKeyError:
+                self.collection.find_one_and_update(
+                    {
+                        "link": home["link"]
+                    },
+                    {
+                        "$set":
+                        {
+                            "updated_at": datetime.utcnow().isoformat()
+                        }
+                    }
+                )
+            except:
+                print("unexepected error occurs")
+                sys.exit()
